1570ed8000a3f16da94fd99306ca1b63b91ca703
Akira Matsuda <ronnie@dio.jp>
2012-09-05 13:56:55 +0900

    initial implementation

--- gitrockets.gemspec
+++ gitrockets.gemspec
@@ -16,4 +16,7 @@ Gem::Specification.new do |gem|
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.require_paths = ["lib"]
+
+  gem.add_dependency 'grit', ['>= 2.5.0']
+  gem.add_dependency 'sprockets', ['>= 2']
 end
--- lib/gitrockets.rb
+++ lib/gitrockets.rb
@@ -1,5 +1,61 @@
 require "gitrockets/version"
+require 'sprockets'
 
 module Gitrockets
-  # Your code goes here...
+  class GitDigest
+    def initialize(fallback_digest)
+      @fallback_digest = fallback_digest
+#       (Grit::Repo.new(Rails.root).tree / 'app/assets').contents.first.id
+      @root = Grit::Repo.new(Rails.root).tree
+      @trees = {}
+    end
+
+    def hexdigest() nil; end
+
+    def file(name)
+#       puts "digesting #{name}..."
+      path = name.sub("#{Rails.root.to_s}/", '')
+      basename = File.basename path
+      tree = tree(File.dirname path)
+      return @fallback_digest.file(name) unless tree
+
+      id = tree.contents.detect {|c| c.basename == basename }.try(:id)
+      Gitrockets::Digest.new id
+    end
+
+    private
+    def tree(dir)
+      @trees[dir] || (@trees[dir] = @root / dir)
+    end
+  end
+
+  class Digest
+    def initialize(val)
+      @val = val
+    end
+
+    def hexdigest
+      @val
+    end
+
+    def to_s
+      @val
+    end
+  end
+end
+
+module Sprockets
+  class Base
+    def git_digest
+      @git_digest ||= ::Gitrockets::GitDigest.new digest
+    end
+
+    def file_digest(path)
+      if (stat = self.stat(path))
+        if stat.file? || stat.directory?
+          git_digest.file(path.to_s)
+        end
+      end
+    end
+  end
 end
